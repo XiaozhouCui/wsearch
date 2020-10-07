9860b03151e563918de5a58a5bbc6991f855a824
Joe Cui <joe.cui@bpanz.com>
2020-10-07 19:47:36 +1000

    Generated empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    PageListComponent
   ],
   imports: [
     BrowserModule
