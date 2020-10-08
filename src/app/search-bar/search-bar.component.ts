ddb9aaf1370ac4debd881be5f6073b14be5b95c1
Joe Cui <joe.cui@bpanz.com>
2020-10-08 21:31:54 +1000

    Added wikipedia service

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    console.log(this.term);
   }
-
 }
