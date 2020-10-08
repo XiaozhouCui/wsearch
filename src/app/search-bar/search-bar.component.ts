e95074b4dc6c59e1ff96c7ab7e060929e39862ad
Joe Cui <joe.cui@bpanz.com>
2020-10-08 21:48:17 +1000

    Child to Parent Communication via @Output and EventEmitter

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  // Create event emitter: send out information as a string
+  @Output() submitted = new EventEmitter<string>(); // typescript generic annotation
   term = '';
 
   constructor() {}
@@ -14,6 +16,6 @@ export class SearchBarComponent implements OnInit {
 
   onFormSubmit(event: any) {
     event.preventDefault();
-    console.log(this.term);
+    this.submitted.emit(this.term);
   }
 }
