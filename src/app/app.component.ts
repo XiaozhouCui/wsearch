398bf0bd275a9677f06ba525cf48054c70f88ae3
Joe Cui <joe.cui@bpanz.com>
2020-10-09 20:10:44 +1000

    Added HttpClient using dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private wikipedia: WikipediaService) {}
+
   onTerm(term: string) {
-    console.log('I am the app and here is the term', term);
+    this.wikipedia.search(term).subscribe((response) => {
+      console.log(response);
+    });
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WikipediaService {
+  // dependency injection: pass in http instance as constructor parameter
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  search(term: string) {
+    // access the injected http dependency
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*', // CORS
+      },
+      // https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${term}
+    });
+  }
 }
